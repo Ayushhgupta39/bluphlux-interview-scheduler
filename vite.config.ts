375f38f63ed2096f5e612e88331be7ed1b4f78d8
Ayush Gupta <ayushgupta3902@gmail.com>
2025-02-12 19:11:26 +0530

    feat: set vercel plugin for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react'
 import path from "path"
 import { defineConfig } from "vite"
+import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
